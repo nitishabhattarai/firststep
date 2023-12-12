f4b367a7e67662e7c6e555b642511a8c2475edea
Nitisha Bhattarai <nitishaabhattarai@gmail.com>
2023-12-12 12:41:34 +0545

    typescript added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import './index.scss';
+import App from './App.tsx';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<App />);
